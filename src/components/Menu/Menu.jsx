121e7f973877ba38c983045c6352b0152a41e753
AlexTicovschi <alexmticovschi@gmail.com>
2020-01-04 11:56:02 +0000

    optimizing images using gatsby-image

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,73 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
 import "./Menu.scss"
 
-import starter from "../../images/home-menu/starter.jpg"
-import duck from "../../images/home-menu/duck.jpg"
-import salad from "../../images/home-menu/salad.jpg"
-import drinks from "../../images/home-menu/drinks.jpg"
-import burger from "../../images/home-menu/burger.jpg"
-import bites from "../../images/home-menu/bites.jpg"
-import pizza from "../../images/home-menu/pizza.jpg"
-import sweets from "../../images/home-menu/sweets.jpg"
+const getImages = graphql`
+  query images {
+    starter: file(relativePath: { eq: "home-menu/starter.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    duck: file(relativePath: { eq: "home-menu/duck.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    salad: file(relativePath: { eq: "home-menu/salad.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    drinks: file(relativePath: { eq: "home-menu/drinks.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    burger: file(relativePath: { eq: "home-menu/burger.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    bites: file(relativePath: { eq: "home-menu/bites.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    pizza: file(relativePath: { eq: "home-menu/pizza.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    sweets: file(relativePath: { eq: "home-menu/sweets.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 const Menu = () => {
+  const images = useStaticQuery(getImages)
+  const { starter, duck, salad, drinks, burger, bites, pizza, sweets } = images
+
   return (
     <section className="menu">
       <h1 className="menu__title">
@@ -19,36 +76,68 @@ const Menu = () => {
 
       <div className="menu__list">
         <figure>
-          <img className="menu__img" src={starter} alt="" />
+          <Image
+            fluid={starter.childImageSharp.fluid}
+            className="menu__img"
+            alt="menu"
+          />
           <h2 className="menu__name">Starters</h2>
         </figure>
         <figure>
-          <img className="menu__img" src={duck} alt="" />
+          <Image
+            fluid={duck.childImageSharp.fluid}
+            className="menu__img"
+            alt="menu"
+          />
           <h2 className="menu__name">Main Course</h2>
         </figure>
         <figure>
-          <img className="menu__img" src={salad} alt="" />
+          <Image
+            fluid={salad.childImageSharp.fluid}
+            className="menu__img"
+            alt="menu"
+          />
           <h2 className="menu__name">Salads</h2>
         </figure>
         <figure>
-          <img className="menu__img" src={drinks} alt="" />
+          <Image
+            fluid={drinks.childImageSharp.fluid}
+            className="menu__img"
+            alt="menu"
+          />
           <h2 className="menu__name">Drinks</h2>
         </figure>
 
         <figure>
-          <img className="menu__img" src={burger} alt="" />
+          <Image
+            fluid={burger.childImageSharp.fluid}
+            className="menu__img"
+            alt="menu"
+          />
           <h2 className="menu__name">Burgers</h2>
         </figure>
         <figure>
-          <img className="menu__img" src={bites} alt="" />
+          <Image
+            fluid={bites.childImageSharp.fluid}
+            className="menu__img"
+            alt="menu"
+          />
           <h2 className="menu__name">One bites</h2>
         </figure>
         <figure>
-          <img className="menu__img" src={pizza} alt="" />
+          <Image
+            fluid={pizza.childImageSharp.fluid}
+            className="menu__img"
+            alt="menu"
+          />
           <h2 className="menu__name">Pizza</h2>
         </figure>
         <figure>
-          <img className="menu__img" src={sweets} alt="" />
+          <Image
+            fluid={sweets.childImageSharp.fluid}
+            className="menu__img"
+            alt="menu"
+          />
           <h2 className="menu__name">Sweets</h2>
         </figure>
       </div>
