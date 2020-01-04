6c79e1642360a86b7cf314cf035fc859819550b5
AlexTicovschi <alexmticovschi@gmail.com>
2020-01-04 12:21:56 +0000

    renaming classes

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -4,9 +4,9 @@ import "./HeroBanner.scss"
 
 const HeroBanner = ({ title, subtitle, children }) => {
   return (
-    <div className="banner">
-      <h1 className="banner__title">{title}</h1>
-      <p className="banner__subtitle">{subtitle}</p>
+    <div className="hero-banner">
+      <h1 className="hero-banner__title">{title}</h1>
+      <p className="hero-banner__subtitle">{subtitle}</p>
       {children}
     </div>
   )
