046376b818f71f02d2bcb723a08e95005b34a3b5
AlexTicovschi <alexmticovschi@gmail.com>
2020-01-05 14:10:18 +0000

    building BigDay component

---
+++ src/components/EventsComponents/Weddings/BigDay/BigDay.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import "./BigDay.scss"
+
+const BigDay = () => {
+  return (
+    <section className="bigday">
+      <h1 className="bigday__title">Big-Day Wow</h1>
+      <p className="bigday__text">
+        Are you thinking of an intimate gathering, or an everyone-is-there
+        affair? Maybe you’re planning something in between? Whether you’re
+        looking for wedding catering in London, or a caterer in Manchester or
+        Liverpool, we’ll make it perfect for you.
+      </p>
+    </section>
+  )
+}
+
+export default BigDay
