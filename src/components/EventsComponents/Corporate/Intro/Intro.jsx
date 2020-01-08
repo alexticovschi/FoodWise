14ef06e37745f54fd0919b0ed41e74cf4434f9df
AlexTicovschi <alexmticovschi@gmail.com>
2020-01-08 17:34:55 +0000

    making corporate event page responsive

--- src/components/EventsComponents/Corporate/Intro/Intro.jsx
+++ src/components/EventsComponents/Corporate/Intro/Intro.jsx
@@ -1,19 +1,18 @@
-import React from "react"
+import React from 'react';
 
-import "./Intro.scss"
+import './Intro.scss';
 
 const Intro = () => {
-  return (
-    <section className="intro">
-      <h1 className="intro__title">We’re the Towns Corporate Caterers</h1>
-      <p className="intro__text">
-        Corporate event planning is one of our specialties. Businesses turn to
-        us again and again for our farm-to-table cuisine, outstanding service
-        and stylish presentations. There’s a reason we’re one of the Central
-        London area’s top catering companies.
-      </p>
-    </section>
-  )
-}
+	return (
+		<section className='corporate-intro'>
+			<h1 className='corporate-intro__title'>We’re the Towns Corporate Caterers</h1>
+			<p className='corporate-intro__text'>
+				Corporate event planning is one of our specialties. Businesses turn to us again and again for our
+				farm-to-table cuisine, outstanding service and stylish presentations. There’s a reason we’re one of the
+				Central London area’s top catering companies.
+			</p>
+		</section>
+	);
+};
 
-export default Intro
+export default Intro;
--- src/components/EventsComponents/Corporate/Stories/Stories.jsx
+++ src/components/EventsComponents/Corporate/Stories/Stories.jsx
@@ -1,62 +1,62 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import StoryCard from "../../StoryCard/StoryCard"
-import "./Stories.scss"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import StoryCard from '../../StoryCard/StoryCard';
+import './Stories.scss';
 
 const getStoryImages = graphql`
-  query storyImages {
-    story1: file(relativePath: { eq: "events/corporate/stories/story1.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    story2: file(relativePath: { eq: "events/corporate/stories/story2.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    story3: file(relativePath: { eq: "events/corporate/stories/story3.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
+	query storyImages {
+		story1: file(relativePath: { eq: "events/corporate/stories/story1.jpg" }) {
+			childImageSharp {
+				fluid(maxWidth: 1000) {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+		story2: file(relativePath: { eq: "events/corporate/stories/story2.jpg" }) {
+			childImageSharp {
+				fluid(maxWidth: 1000) {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+		story3: file(relativePath: { eq: "events/corporate/stories/story3.jpg" }) {
+			childImageSharp {
+				fluid(maxWidth: 1000) {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+	}
+`;
 
 const Stories = () => {
-  const { story1, story2, story3 } = useStaticQuery(getStoryImages)
+	const { story1, story2, story3 } = useStaticQuery(getStoryImages);
 
-  return (
-    <section className="stories">
-      <h1 className="stories__title">Experience a Story</h1>
-      <div className="stories__cards">
-        <StoryCard
-          url="/events/corporate"
-          imgSrc={story1}
-          name="Uncontainable Fun"
-          text="Creativity was key at a super-organized and festive celebration at The Container Store."
-        />
-        <StoryCard
-          url="/events/corporate"
-          imgSrc={story2}
-          name="Deliciously Healthy"
-          text="The best way to welcome a new health center? Design a menu with good-for-you cuisine."
-        />
-        <StoryCard
-          url="/events/corporate"
-          imgSrc={story3}
-          name="A Business Birthday"
-          text="After 50 years of business, employees deserve a big ol’ party and a good ol’ time."
-        />
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className='event-stories'>
+			<h1 className='event-stories__title'>Experience a Story</h1>
+			<div className='event-stories__cards'>
+				<StoryCard
+					url='/events/corporate'
+					imgSrc={story1}
+					name='Uncontainable Fun'
+					text='Creativity was key at a super-organized and festive celebration at The Container Store.'
+				/>
+				<StoryCard
+					url='/events/corporate'
+					imgSrc={story2}
+					name='Deliciously Healthy'
+					text='The best way to welcome a new health center? Design a menu with good-for-you cuisine.'
+				/>
+				<StoryCard
+					url='/events/corporate'
+					imgSrc={story3}
+					name='A Business Birthday'
+					text='After 50 years of business, employees deserve a big ol’ party and a good ol’ time.'
+				/>
+			</div>
+		</section>
+	);
+};
 
-export default Stories
+export default Stories;
